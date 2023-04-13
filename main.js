a756bc6866b66132eb6654f7bbb7c66b02a25b36
Martin Loria <martinloria74@gmail.com>
2023-04-13 15:40:52 -0600

    Integracion del menu movil

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('oculto');
+}
+
+/*
+const ocultar = (element) => {
+    element.classList.toggle('inactive');
+}
+
+menuEmail.addEventListener('click',()=>{
+    ocultar(desktopMenu);
+});
+menuHamIcon.addEventListener('click',()=>{
+    ocultar(mobileMenu);
+}); */
