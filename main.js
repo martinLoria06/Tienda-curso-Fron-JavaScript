9d5ada2f1b73fd3a1dbe7328a837ed8ce83207ae
Martin Loria <martinloria74@gmail.com>
2023-04-13 17:54:06 -0600

    Menu de carrito de compras y ocultar menus cuando se abra otro

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
-menuEmail.addEventListener('click',toggleDesktopMenu);
-menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  desktopMenu.classList.toggle("inactive");
 }
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('oculto');
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("oculto");
+  /*
+  if (aside.className.includes("inactive") != true) {
+    aside.classList.add("inactive");
+  } */
 }
 
+function toggleCarritoAside() {
+  /*Una alternativa para validar */
+  const isMobilClosed = mobileMenu.classList.contains("oculto");
+  const isDestockMenu = desktopMenu.classList.contains("inactive");
+
+  if (!isMobilClosed) {
+    mobileMenu.classList.add("oculto");
+  }
+
+  if(!isDestockMenu){
+    desktopMenu.classList.add('inactive');
+  }
+  aside.classList.toggle("inactive");
+
+  /* console.log(mobileMenu.className.includes('oculto'));
+    if(mobileMenu.className.includes('oculto') != true){
+        mobileMenu.classList.add('oculto');
+    }else {
+        console.log(mobileMenu.className);
+    } */
+}
+
+/* Una alternativa para reutlizar el codigo */
 /*
 const ocultar = (element) => {
     element.classList.toggle('inactive');
